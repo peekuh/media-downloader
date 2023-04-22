2ee41a483ddabf08f5b98b7298ddb5080012f1d8
Srijan Sarthak <sar.srijan@gmail.com>
2023-04-22 15:32:53 +0530

    added gallery-dl functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,11 +22,26 @@ void MainWindow::on_download_button_clicked()
     QString url = lineEdit->text();
     QStringList args;
     args << url;
-    QString program = R"(C:\\Users\\sarsr.LAPTOP-65K8MNNO\\scoop\\apps\\yt-dlp\\2023.03.04\\yt-dlp.exe)";
-    QProcess process;
-    process.start(program, args);
-    process.waitForFinished(-1);
 
-    QMessageBox::information(this, "Message", "Download complete.");
+    QComboBox *combo = this->findChild<QComboBox*>("engine_combobox");
+    QString combo_text = combo->currentText();
+    QString str = "twitter";
+    if (combo_text == str)
+    {
+        QString gallery_dl = R"(C:\Users\sarsr.LAPTOP-65K8MNNO\scoop\apps\gallery-dl\1.25.2\gallery-dl.exe)";
+        QProcess twitter;
+        twitter.start(gallery_dl, args);
+        twitter.waitForFinished(-1);
+        QMessageBox::information(this, "Message", "Download complete.");
+    }
+    else
+    {
+        QString program = R"(C:\\Users\\sarsr.LAPTOP-65K8MNNO\\scoop\\apps\\yt-dlp\\2023.03.04\\yt-dlp.exe)";
+        QProcess process;
+        process.start(program, args);
+        process.waitForFinished(-1);
+
+        QMessageBox::information(this, "Message", "Download complete.");
+    }
 }
 
